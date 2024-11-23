6be4060c7b7e33d84eb69719987ec237e8e87d55
Sapiens-wx <wx2194@nyu.edu>
2024-11-23 03:15:26 +0000

    ls -l -a and specify directory

--- lab2/main.c
+++ lab2/main.c
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
+#include <dirent.h>
 
 static int err_code;
 
@@ -125,6 +126,12 @@ void handle_error(char* what_happened, char* fullname) {
     PRINT_ERROR("ls", what_happened, fullname);
 
     // TODO: your code here: inspect errno and set err_code accordingly.
+	err_code|=0x80;
+	switch(errno){
+		case ENOENT: err_code|=1; break;
+		case EACCES: err_code|=2; break;
+		default: err_code|=2; break;
+	}
     return;
 }
 
@@ -149,15 +156,31 @@ bool test_file(char* pathandname) {
  */
 bool is_dir(char* pathandname) {
     /* TODO: fillin */
-
-    return false;
+	struct stat info;
+	stat(pathandname, &info);
+	return (S_IFMT&info.st_mode)==S_IFDIR;
 }
 
 /* convert the mode field in a struct stat to a file type, for -l printing */
 const char* ftype_to_str(mode_t mode) {
     /* TODO: fillin */
+	static char modestr[11];
+	modestr[0]=((mode&S_IFMT)==S_IFDIR)?'d':'-';
+	//owner
+	modestr[1]=(mode&0400)?'r':'-';
+	modestr[2]=(mode&0200)?'w':'-';
+	modestr[3]=(mode&0100)?'x':'-';
+	//group
+	modestr[4]=(mode&040)?'r':'-';
+	modestr[5]=(mode&020)?'w':'-';
+	modestr[6]=(mode&010)?'x':'-';
+	//others
+	modestr[7]=(mode&04)?'r':'-';
+	modestr[8]=(mode&02)?'w':'-';
+	modestr[9]=(mode&01)?'x':'-';
 
-    return "?";
+	modestr[10]=0;
+	return modestr;
 }
 
 /* list_file():
@@ -176,6 +199,25 @@ const char* ftype_to_str(mode_t mode) {
  */
 void list_file(char* pathandname, char* name, bool list_long) {
     /* TODO: fill in*/
+	if(list_long){
+		struct stat info;
+		if(stat(pathandname, &info)==-1){
+			handle_error("cannot open file", name);
+			return;
+		}
+		//date string
+#define DATE_BUF_SIZE 16
+		char datestr[DATE_BUF_SIZE];
+		date_string(&info.st_mtim, datestr, DATE_BUF_SIZE);
+#define ID_BUF_SIZE 10
+		char uname[ID_BUF_SIZE], group[ID_BUF_SIZE];
+		uname_for_uid(info.st_uid, uname, ID_BUF_SIZE);
+		group_for_gid(info.st_gid, group, ID_BUF_SIZE);
+		//isDir, modes, mode number, user, group, modified time, size,
+		printf("%s %4d %*s %*s %*s %6ld ", ftype_to_str(info.st_mode), (info.st_mode&S_IFMT)>>14, ID_BUF_SIZE, uname, ID_BUF_SIZE, group, DATE_BUF_SIZE, datestr, info.st_size);
+#undef ID_BUF_SIZE
+	}
+	printf("%s\n", name);
 }
 
 /* list_dir():
@@ -198,6 +240,20 @@ void list_dir(char* dirname, bool list_long, bool list_all, bool recursive) {
      *       closedir()
      *   See the lab description for further hints
      */
+	DIR* dir=opendir(dirname);
+	if(dir==NULL){
+		handle_error("cannot open directory",dirname);
+		return;
+	}
+	struct dirent* entry;
+	while((entry=readdir(dir))){
+		char pathandname[512];
+		if(list_all||entry->d_name[0]!='.'){
+			snprintf(pathandname, 512, "%s/%s", dirname, entry->d_name);
+			list_file(pathandname, entry->d_name, list_long);
+		}
+	}
+	closedir(dir);
 }
 
 int main(int argc, char* argv[]) {
@@ -205,33 +261,41 @@ int main(int argc, char* argv[]) {
     // unsigned.
     int opt;
     err_code = 0;
-    bool list_long = false, list_all = false;
+    bool list_long = false, list_all = false, list_recursive = false;
     // We make use of getopt_long for argument parsing, and this
     // (single-element) array is used as input to that function. The `struct
     // option` helps us parse arguments of the form `--FOO`. Refer to `man 3
     // getopt_long` for more information.
     struct option opts[] = {
-        {.name = "help", .has_arg = 0, .flag = NULL, .val = '\a'}};
+        {.name = "help", .has_arg = 1, .flag = NULL, .val = '\a'}};
 
     // This loop is used for argument parsing. Refer to `man 3 getopt_long` to
     // better understand what is going on here.
-    while ((opt = getopt_long(argc, argv, "1a", opts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "1laRr", opts, NULL)) != -1) {
         switch (opt) {
             case '\a':
                 // Handle the case that the user passed in `--help`. (In the
                 // long argument array above, we used '\a' to indicate this
                 // case.)
+				printf("--help, arg=%s\n", optarg);
                 help();
                 break;
             case '1':
                 // Safe to ignore since this is default behavior for our version
                 // of ls.
                 break;
+			case 'l':
+				list_long=true;
+				break;
             case 'a':
                 list_all = true;
                 break;
                 // TODO: you will need to add items here to handle the
                 // cases that the user enters "-l" or "-R"
+			case 'r':
+			case 'R':
+				list_recursive=true;
+				break;
             default:
                 printf("Unimplemented flag %d\n", opt);
                 break;
@@ -239,16 +303,16 @@ int main(int argc, char* argv[]) {
     }
 
     // TODO: Replace this.
-    if (optind < argc) {
-        printf("Optional arguments: ");
-    }
-    for (int i = optind; i < argc; i++) {
-        printf("%s ", argv[i]);
-    }
-    if (optind < argc) {
-        printf("\n");
-    }
+	//if there is no directory specified
+    if (optind >= argc) {
+		list_dir(".",list_long,list_all,list_recursive);
+    }else{
+		for (int i = optind; i < argc; i++) {
+			printf("%s:\n", argv[i]);
+			list_dir(argv[i],list_long,list_all,list_recursive);
+			putchar('\n');
+		}
+	}
 
-    NOT_YET_IMPLEMENTED("Listing files");
     exit(err_code);
 }
